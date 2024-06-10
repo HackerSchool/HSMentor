b38b4f13f1a8e4eae9f2cc228c6db1ab44bed3ed
Daniel Carvalho <daniel.m.carvalho@tecnico.ulisboa.pt>
2024-06-10 16:03:21 +0100

    refactor(bottom bar): Put bottom bar in an isolated file, so it can be reused in various pages, not related to its navigation

--- lib/pages/pagina_principal.dart
+++ lib/pages/pagina_principal.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:hsmentor/components/barra_navegacao.dart';
 import 'package:hsmentor/pages/faq.dart';
 import 'package:hsmentor/pages/home.dart';
 import 'package:hsmentor/pages/navegacao.dart';
@@ -56,44 +56,9 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
 
       body: routes[_selectedIndex],
 
-      bottomNavigationBar: Container(
-          color: Color(0xFF6BBA75),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-            child: GNav(
-              gap: 6,
-              selectedIndex: 2,
-              onTabChange: (index) => navigateBottomBar(index),
-              backgroundColor: Color(0xFF6BBA75),
-              color: Colors.white,
-              activeColor: Color(0xFF6BBA75),
-              tabBackgroundColor: Colors.white,
-              padding: EdgeInsets.all(16),
-              tabs: const [
-                GButton(
-                  icon: Icons.question_mark,
-                  text: "FAQ",
-                ),
-                GButton(
-                  icon: Icons.restaurant,
-                  text: "Refeições",
-                ),
-                GButton(
-                  icon: Icons.home,
-                  text: "Home",
-                ),
-                GButton(
-                  icon: Icons.navigation,
-                  text: "Navegação",
-                ),
-                GButton(
-                  icon: Icons.newspaper,
-                  text: "Notícias",
-                )
-              ],
-            ),
-          ),
-        ),
+      bottomNavigationBar: BarraNavegacao(
+        onTabChange: (index) => navigateBottomBar(index),
+      )
     );
   }
 }

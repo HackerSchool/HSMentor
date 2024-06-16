fffb247be29e1830cf9b0198989c55db18792908
Daniel Carvalho <daniel.m.carvalho2002@gmail.com>
2024-06-16 13:56:53 +0100

    feat(faq): Added faq page and tile

--- lib/pages/faq.dart
+++ lib/pages/faq.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:hsmentor/components/faq_tile.dart';
 
 class FAQ extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Center(
+      child: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return FaqTile();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

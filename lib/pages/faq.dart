7b2266988b5e9caa1c720f5e32ba431118de64c2
Daniel Carvalho <daniel.m.carvalho2002@gmail.com>
2024-06-16 15:44:01 +0100

    feat(html): Implemented html files in dropdown of faq

--- lib/pages/faq.dart
+++ lib/pages/faq.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:hsmentor/components/faq_tile.dart';
 
 class FAQ extends StatelessWidget {
+
+  List<String> titles = ["Pergunta 1", "Pergunta 2", "Pergunta 3", "Pergunta 4"];
+  List<String> fileName = ["file1.html", "file2.html", "file3.html", "file4.html"];
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -11,7 +15,10 @@ class FAQ extends StatelessWidget {
             child: ListView.builder(
               itemCount: 4,
               itemBuilder: (context, index) {
-                return FaqTile();
+                return FaqTile(
+                  title:titles[index], 
+                  fileName: fileName[index],
+                );
               },
             ),
           ),
